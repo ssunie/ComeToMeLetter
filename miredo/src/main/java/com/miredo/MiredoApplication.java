4b8f677b682dfc6fe62f76f8310c0d915440d130
sunie <ks670711@gmail.com>
2023-10-26 01:27:04 +0900

    Create Project

---
+++ miredo/src/main/java/com/miredo/MiredoApplication.java
@@ -0,0 +1,13 @@
+package com.miredo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiredoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiredoApplication.class, args);
+	}
+
+}
---
+++ miredo/src/test/java/com/miredo/MiredoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miredo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiredoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
