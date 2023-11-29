6718bc56ce9016dac162d0358076ee2a986eeac8
sunie <ks670711@gmail.com>
2023-11-30 00:08:29 +0900

    application setting

--- miredo/src/main/java/com/miredo/MiredoApplication.java
+++ miredo/src/main/java/com/miredo/MiredoApplication.java
@@ -1,7 +1,14 @@
 package com.miredo;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
 public class MiredoApplication {
@@ -9,5 +16,17 @@ public class MiredoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MiredoApplication.class, args);
 	}
+	
+	 
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml");
+        sessionFactory.setMapperLocations(res);
+        
+        return sessionFactory.getObject();
+    }
 
 }
