0e2cc4844a4720b01a00d32755cdd5c7525a9827
sunie <ks670711@gmail.com>
2024-01-04 23:41:56 +0900

    update service

--- miredo/src/main/java/com/miredo/service/UserMainService.java
+++ miredo/src/main/java/com/miredo/service/UserMainService.java
@@ -1,26 +1,36 @@
 package com.miredo.service;
 
-import java.util.List;
-import java.util.Map;
 
 import com.miredo.model.dto.UserDTO;
 
 public interface UserMainService {
 	
+	//회원 아이디로 조회
+	UserDTO findUserById(String name);
+	
+	// 아이디 찾기
+	String findIdByName(String name, String email);
+	
+	// 아이디 중복 체크
 	int checkId(UserDTO user);
 	
-	public List<Map<String, Object>> SelectAllList() throws Exception;
-	
-	int deleteUser(String id, String active);
-	
-	String findIdByName(String name, String email);
-
+	//비밀번호 찾기
 	UserDTO findPwd(UserDTO user);
 
+	//비밀번호 변경
 	int updatePwd(UserDTO user);
-
+	
+	//현재 비밀번호 확인
 	String findPwdById(String id);
 	
+	//회원 등록
+	int insertUser(UserDTO user);
+	
+	//회원 정보 수정
+	int updateUser(UserDTO user);
+	
+	//회원 삭제
+	int deleteUser(String id, String active);
 	
 
 }
