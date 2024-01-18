fec12352c8c4dff39c1ac0ac4c44bbc535984561
sunie <ks670711@gmail.com>
2024-01-18 23:59:12 +0900

    update usermainserviceimpl

--- miredo/src/main/java/com/miredo/service/UserMainServiceImpl.java
+++ miredo/src/main/java/com/miredo/service/UserMainServiceImpl.java
@@ -44,7 +44,7 @@ public class UserMainServiceImpl implements UserMainService {
 		log.info("로그인 유저 : {}", user);
 		
 		/* 권한 리스트 */
-		List<GrantedAuthority> authorities = new ArrayList<>();
+		//List<GrantedAuthority> authorities = new ArrayList<>();
 		
 		
 		UserImpl member = new UserImpl(user.getId(), user.getPassword(), authorities);
@@ -53,6 +53,7 @@ public class UserMainServiceImpl implements UserMainService {
 		return member;
 		
 	}
+	
 
 	/* 아이디 찾기 */
 	@Override
