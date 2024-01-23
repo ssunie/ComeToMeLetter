5901e56f6760069b45a2fa5b474898e77f45992d
sunie <ks670711@gmail.com>
2024-01-24 00:10:11 +0900

    Merge branch 'main' of https://github.com/ssunie/miredo.git into main

--- miredo/src/main/java/com/miredo/MiredoApplication.java
+++ miredo/src/main/java/com/miredo/MiredoApplication.java
@@ -17,7 +17,6 @@ public class MiredoApplication {
 		SpringApplication.run(MiredoApplication.class, args);
 	}
 	
-	 .
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
